5464b0d33a735d7c3d918a49c0008ec72cf8c7d9
João Macieira <jp.macieira@uol.com.br>
2021-10-02 12:07:54 -0300

    Criação dos métodos delete e update com validação e acrescimo dos comentários

--- controllers/seriesController.js
+++ controllers/seriesController.js
@@ -1,21 +1,72 @@
-const Serie = require('../models/serie');
+const labels = [
+  { id: 'to-watch', name: 'Para Assistir' },
+  { id: 'watching', name: 'Assistindo' },
+  { id: 'watched', name: 'Assistido' }
+];
 
-const index = (request, response) => {
-  Serie.find({}, (error, docs) => {
-    response.render('series/index', { series: docs });
-  });
+const index = async ({ Serie }, request, response) => {
+  const docs = await Serie.find()
+  response.render('series/index', { series: docs, labels });
 }
 
-const store = (request, response) => {
-  const serie = new Serie({
-    name: 'friends',
-    status: 'watched'
-  });
-  serie.save(() => console.log('saved'));
-  response.render('series/nova');
+const storeProcess = async ({ Serie }, request, response) => {
+  const serie = new Serie(request.body);
+  try {
+    await serie.save();
+    response.redirect('/series');
+  } catch (error) {
+    response.render('series/nova', { errors: Object.keys(error.errors) });
+  }
+}
+
+const storeForm = (request, response) => {
+  response.render('series/nova', { errors: '' });
+}
+
+const editProcess = async ({ Serie }, request, response) => {
+  const serie = await Serie.findOne({ _id: request.params.id });
+  serie.name = request.body.name;
+  serie.status = request.body.status;
+  try {
+    await serie.save();
+    response.redirect('/series');
+  } catch(error) {
+    response.render('series/editar', { serie, labels, errors: Object.keys(error.errors) });
+  }
+
+}
+
+const editForm = async ({ Serie }, request, response) => {
+  const serie = await Serie.findOne({ _id: request.params.id });
+  response.render('series/editar', { serie, labels, errors: '' });
+}
+
+const excluir = async ({Serie}, request, response) => {
+  await Serie.deleteOne({ _id: request.params.id });
+  response.redirect('/series');
+}
+
+const info = async ({ Serie }, request, response) => {
+  const serie = await Serie.findOne({ _id: request.params.id });
+  response.render('series/info', { serie });
+}
+
+const addComments = async ({ Serie }, request, response) => {
+  await Serie.updateOne({
+    _id: request.params.id
+  }, {
+    $push: { comments: request.body.comentario }
+  })
+  response.redirect('/series/info/'+request.params.id);
 }
 
 module.exports = {
   index,
-  store
+  storeProcess,
+  storeForm,
+  editProcess,
+  editForm,
+  excluir,
+  info,
+  addComments
 }
--- routes/series.js
+++ routes/series.js
@@ -3,7 +3,20 @@ const seriesControllers = require('../controllers/seriesController');
 
 const router = express.Router();
 
-router.get('/', seriesControllers.index);
-router.get('/nova', seriesControllers.store);
+const Serie = require('../models/serie');
+
+const models = {
+  Serie
+}
+
+router.get('/', seriesControllers.index.bind(null, models));
+router.get('/nova', seriesControllers.storeForm);
+router.post('/nova', seriesControllers.storeProcess.bind(null, models));
+router.get('/editar/:id', seriesControllers.editForm.bind(null, models));
+router.post('/editar/:id', seriesControllers.editProcess.bind(null, models));
+router.get('/excluir/:id', seriesControllers.excluir.bind(null, models));
+
+router.get('/info/:id', seriesControllers.info.bind(null, models));
+router.post('/info/:id', seriesControllers.addComments.bind(null, models));
 
 module.exports = router;
